2b78f0b981cff8411d2a50daba4f7883822c9064
eediallo <elhhabdouldiallo@gmail.com>
2025-03-14 20:20:57 +0000

    replace Express with native HTTP server for handling home and about pages with 404 response

--- 02-express-tutorial/final/01-http-basics.js
+++ 02-express-tutorial/final/01-http-basics.js
@@ -1,7 +1,24 @@
-import express from 'express'
+const server = http.createServer((req, res) => {
+  // console.log(req.method)
+  const url = req.url
+  // home page
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>home page</h1>')
+    res.end()
+  }
+  // about page
+  else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>about page</h1>')
+    res.end()
+  }
+  // 404
+  else {
+    res.writeHead(404, { 'content-type': 'text/html' })
+    res.write('<h1>page not found</h1>')
+    res.end()
+  }
+})
 
-const app = express()
-
-
-
-app.listen(3000, ()=> console.log('Server is listening on port 3000....'))
+server.listen(3000, ()=> console.log('Server is listening on port 3000..'))
