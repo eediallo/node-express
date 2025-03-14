f0011d5128b5a5fa02c03168af37cd959f11d3f4
eediallo <elhhabdouldiallo@gmail.com>
2025-03-14 17:42:47 +0000

    add routing for home and about pages with 404 handling

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,10 +2,20 @@
 import http from 'http'
 
 const server = http.createServer((req, res) =>{
-    console.log('User hit the server')
-    res.writeHead(200, {'content-type': 'text/html'})
-    res.write('<p>Welcome to our home page</p>')
-    res.end()
+    const url = req.url
+    if (url === '/'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write('<p>Welcome to our home page</p>')
+        res.end()
+    }else if (url === '/about'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write('<p>This is our history</p>')
+        res.end()
+    } else{
+        res.writeHead(404, {'content-type': 'text/html'})
+        res.write('<p>Page not found</p>')
+        res.end()
+    }
 })
 
 
