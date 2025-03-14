3a788348dac0adabea59cb994d030c7e2af29c8a
eediallo <elhhabdouldiallo@gmail.com>
2025-03-14 17:29:39 +0000

    add response message for home page in HTTP server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,6 +3,7 @@ import http from 'http'
 
 const server = http.createServer((req, res) =>{
     console.log('User hit the server')
+    res.end('This is our home page. Thank you for visiting')
 })
 
 
