d86e00e4737d0b28223a68b16791a4116c915824
eediallo <elhhabdouldiallo@gmail.com>
2025-03-14 21:18:51 +0000

    update server to return product data and handle 404 responses

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,25 +1,14 @@
-import express from 'express'
-import path from 'path'
+import express from "express";
+import { products, people } from "./data.js";
 
-const __dirname = new URL('./navbar-app', import.meta.url).pathname
+const app = express();
 
+app.get("/", (req, res) => {
+  res.json(products);
+});
 
-const app = express()
+app.get("*", (req, res) => {
+  res.status(404).end("No resource found");
+});
 
-// setup static and middleware
-app.use(express.static('./public'))
-
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'index.html'))
-    
-})
-app.all('*', (req, res)=>{
-res.status(404).send('Resource not found.')
-})
-
-// app.get('/', (req, res)=>{
-
-// })
-
-
-app.listen(3000, ()=> console.log('Server is listen on port 3000...'))
+app.listen(3000, () => console.log("Server is running on port 3000..."));
