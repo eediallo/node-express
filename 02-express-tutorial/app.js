48b0efd50f1469b88f06b8c7c712c533410bc2e0
eediallo <elhhabdouldiallo@gmail.com>
2025-03-14 17:24:57 +0000

    set up basic HTTP server and update package configuration

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,9 @@
-console.log('Express Tutorial')
+
+import http from 'http'
+
+const server = http.createServer((req, res) =>{
+    console.log('User hit the server')
+})
+
+
+server.listen(3000, ()=> console.log('Server is running on port 3000...'))
