fd5ec598652a9412c53a83270b06c2799aad3543
eediallo <elhhabdouldiallo@gmail.com>
2025-03-14 17:45:45 +0000

    add handling for home and about pages with 404 response

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,15 +2,21 @@ import http from "http";
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+
+  // home page
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<p>Welcome to our home page</p>");
     res.end();
-  } else if (url === "/about") {
+  } 
+  // about page
+  else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<p>This is our history</p>");
     res.end();
-  } else {
+  }
+  // 404
+  else {
     res.writeHead(404, { "content-type": "text/html" });
     res.write("<p>Page not found</p>");
     res.end();
