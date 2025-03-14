7e8fcbf816f26a0d5b83e866756c4b438a5abb93
eediallo <elhhabdouldiallo@gmail.com>
2025-03-14 20:10:49 +0000

    update server to serve additional assets for home page

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,11 +2,13 @@ import http from "http";
 import { readFileSync } from "fs";
 
 //get files 
-const homePage = readFileSync('./index.html')
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+const homeLogo = readFileSync('./navbar-app/logo.svg')
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-
   // home page
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
@@ -17,6 +19,24 @@ const server = http.createServer((req, res) => {
   else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<p>This is our history</p>");
+    res.end();
+  }
+  // styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+  // logic
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
+    res.end();
+  }
+  // logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeLogo);
     res.end();
   }
   // 404
