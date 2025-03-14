d0f2db2e4afb8a8a64801e8ae0ea42c0dfc5dfe2
eediallo <elhhabdouldiallo@gmail.com>
2025-03-14 17:44:56 +0000

    refactor server code for improved readability and consistency

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,20 @@
+import http from "http";
 
-import http from 'http'
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<p>Welcome to our home page</p>");
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<p>This is our history</p>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<p>Page not found</p>");
+    res.end();
+  }
+});
 
-const server = http.createServer((req, res) =>{
-    const url = req.url
-    if (url === '/'){
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write('<p>Welcome to our home page</p>')
-        res.end()
-    }else if (url === '/about'){
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write('<p>This is our history</p>')
-        res.end()
-    } else{
-        res.writeHead(404, {'content-type': 'text/html'})
-        res.write('<p>Page not found</p>')
-        res.end()
-    }
-})
-
-
-server.listen(3000, ()=> console.log('Server is running on port 3000...'))
+server.listen(3000, () => console.log("Server is running on port 3000..."));
