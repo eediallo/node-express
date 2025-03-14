5f0b8cfb53b25e94c17d8ffcc760fae512009d3b
eediallo <elhhabdouldiallo@gmail.com>
2025-03-14 21:27:02 +0000

    update home route to serve HTML and add API endpoint for products

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,11 +4,19 @@ import { products, people } from "./data.js";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send('<h1>Home page</h1><a href="/api/products">Products</a>')
 });
 
-app.get("*", (req, res) => {
-  res.status(404).end("No resource found");
+app.get("/api/products", (req, res) => {
+    const newProducts = products.map(product =>{
+        const{id, name, image} =  product
+        return {id, name, image}
+    })
+  res.json(newProducts);
 });
+
+// app.get("*", (req, res) => {
+//   res.status(404).end("No resource found");
+// });
 
 app.listen(3000, () => console.log("Server is running on port 3000..."));
