d813446c90e356c5976dfecc8daca18dd04478b4
eediallo <elhhabdouldiallo@gmail.com>
2025-03-14 20:21:16 +0000

    refactor server to use Express for routing and simplify response handling

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,50 +1,9 @@
-import http from "http";
-import { readFileSync } from "fs";
+import express from 'express'
 
-//get files 
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
-const homeLogo = readFileSync('./navbar-app/logo.svg')
+const app = express()
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  // home page
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  } 
-  // about page
-  else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<p>This is our history</p>");
-    res.end();
-  }
-  // styles
-  else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyles);
-    res.end();
-  }
-  // logic
-  else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-    res.end();
-  }
-  // logo
-  else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homeLogo);
-    res.end();
-  }
-  // 404
-  else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<p>Page not found</p>");
-    res.end();
-  }
-});
+app.get('/', (req, res)=>{
+  res.send('Home Page')
+})
 
-server.listen(3000, () => console.log("Server is running on port 3000..."));
+app.listen(3000, ()=> console.log('Server is listening on port 3000....'))
