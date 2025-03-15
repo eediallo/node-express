b82bde2cd88c997fa4d47830ab65c16d40d4d464
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-15 06:53:14 +0000

    feat: serve static assets from methods-public directory

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,13 @@
 import express from "express";
 import {people} from './data.js'
+import path from 'path'
+
+const staticDIr = new URL('./methods-public', import.meta.url).pathname
 
 const app = express(); 
+
+// static assets
+app.use(express.static((staticDIr)))
 
 app.get('/api/people', (req, res) =>{
   res.status(200).json({success: true, people: people})
