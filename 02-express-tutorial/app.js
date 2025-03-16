ca3dac6fd8b66ef6670947bde423f0647fd260f9
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-16 15:13:15 +0000

    feat: add PUT /api/people/:id endpoint for updating person data

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -29,7 +29,6 @@ app.post("/api/people", (req, res) => {
   res.status(201).json({ success: true, name: name });
 });
 
-
 app.post("/api/postman/people", (req, res) => {
   console.log(req.body);
   const { name, id } = req.body;
@@ -38,8 +37,31 @@ app.post("/api/postman/people", (req, res) => {
     res.status(400).json({ success: false, name: name });
   }
 
-  res.status(201).json({ success: true, data: [...people, {name: name, id: id}] });
+  res
+    .status(201)
+    .json({ success: true, data: [...people, { name: name, id: id }] });
 });
 
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    console.error(`Person with ${id} not found`);
+    res
+      .status(404)
+      .json({ success: false, msg: `Person with ${id} not found` });
+  }
+
+  const updatePeopleList = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+
+  res.status(200).json({ success: true, data: updatePeopleList });
+});
 
 app.listen(3000, () => console.log("Server is running on port 3000..."));
