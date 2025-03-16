45ebeba967bd7b183383689e33618ea9800a27dd
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-16 14:49:31 +0000

    feat: add /api/postman/people route and POST /api/people endpoint for product handling

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,15 +1,32 @@
 import express from "express";
-import {people} from './data.js'
+import { people, products } from "./data.js";
 
-const staticDIr = new URL('./methods-public', import.meta.url).pathname
+const staticDIr = new URL("./methods-public", import.meta.url).pathname;
 
-const app = express(); 
+const app = express();
 
 // static assets
-app.use(express.static((staticDIr)))
+app.use(express.static(staticDIr));
 
-app.get('/api/people', (req, res) =>{
-  res.status(200).json({success: true, people: people})
-})
+app.use(express.json());
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, people: people });
+});
+
+app.get("/api/postman/people", (req, res) => {
+  res.status(200).json({ success: true, products: products });
+});
+
+app.post("/api/people", (req, res) => {
+  console.log(req.body);
+  const { name } = req.body;
+  if (!name) {
+    console.error(`Name must be provided`);
+    res.status(400).json({ success: false, name: name });
+  }
+
+  res.status(201).json({ success: true, name: name });
+});
 
 app.listen(3000, () => console.log("Server is running on port 3000..."));
