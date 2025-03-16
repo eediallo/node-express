a71503b28fb7fb7baa03ad6021dc30d5ab041436
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-16 14:59:17 +0000

    feat: add POST /api/postman/people endpoint for handling people data

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -29,4 +29,17 @@ app.post("/api/people", (req, res) => {
   res.status(201).json({ success: true, name: name });
 });
 
+
+app.post("/api/postman/people", (req, res) => {
+  console.log(req.body);
+  const { name, id } = req.body;
+  if (!name) {
+    console.error(`Name must be provided`);
+    res.status(400).json({ success: false, name: name });
+  }
+
+  res.status(201).json({ success: true, data: [...people, {name: name, id: id}] });
+});
+
+
 app.listen(3000, () => console.log("Server is running on port 3000..."));
