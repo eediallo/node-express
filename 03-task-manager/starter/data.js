d6fdb49611973ac719fe7ed6c29d681f0dfa6812
eediallo <elhhabdouldiallo@gmail.com>
2025-03-17 06:17:20 +0000

    feat: add initial task data for task management

---
+++ 03-task-manager/starter/data.js
@@ -0,0 +1,20 @@
+const tasks = [
+  {
+    task: "Go shopping",
+    id: 1,
+  },
+  {
+    task: "Walk the dog",
+    id: 2,
+  },
+  {
+    task: "Edit video",
+    id: 3,
+  },
+  {
+    task: "Wash the dog",
+    id: 4,
+  },
+];
+
+export { tasks };
