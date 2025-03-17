f95020bbdc73f3b72cb53be857c1623c62c64235
eediallo <elhhabdouldiallo@gmail.com>
2025-03-17 15:41:02 +0000

    feat: refactor getSingleTask to use async/await and improve error handling

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -12,7 +12,7 @@ const fetchTasksFromDB = async () => {
 
 // get all tasks
 const getAllTasks = async (req, res) => {
-  const tasks = await fetchTasksFromDB ();
+  const tasks = await fetchTasksFromDB();
   if (tasks.length === 0) {
     return res.status(404).json({ success: false, msg: "No task found." });
   }
@@ -21,7 +21,6 @@ const getAllTasks = async (req, res) => {
 
 // create a task
 const createTask = async (req, res) => {
-  console.log(req.body);
   try {
     const task = await Task.create(req.body);
     res.status(201).json({ success: true, task: task });
@@ -31,15 +30,19 @@ const createTask = async (req, res) => {
 };
 
 // get single task
-const getSingleTask = (req, res) => {
-  const { id } = req.params;
-  const task = tasks.find((task) => task.id === Number(id));
-  if (!task) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `Task with id ${Number(id)} not found` });
+const getSingleTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  try {
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ success: false, msg: `Task with id ${taskID} not found` });
+    }
+    res.status(200).json({ success: true, task: task });
+  } catch (err) {
+    res.status(404).json({ success: false, msg: err });
   }
-  res.status(200).json({ success: true, task: task });
 };
 
 // update task
