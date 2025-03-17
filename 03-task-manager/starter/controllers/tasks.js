a08d74b14ab2f59bea2e162b99abf99fc1e716c7
eediallo <elhhabdouldiallo@gmail.com>
2025-03-17 06:05:47 +0000

    add functionnality for creating a task

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,7 +1,25 @@
-const getTasks = (req, res) => {
+const getAllTasks = (req, res) => {
   res.send("All items");
 };
 
 
+const createTask = (req, res) =>{
+    const {task} = req.body
+    if(!task){
+        res.status(400).json({success: false, msg: 'Fail to create task'})
+    }
+    res.status(201).json({success: true, task: task})
+}
+const getSingleTask = (req, res) =>{
+    res.send('Get single  task')
+}
+const updateTask = (req, res) =>{
+    res.send('Update task')
+}
+const deleteTask = (req, res) =>{
+    res.send('delete task')
+}
 
-export {getTasks}
+
+
+export {getAllTasks, createTask, getSingleTask, updateTask, deleteTask}
