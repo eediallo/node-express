2d39d28804b062ce3a4a563bf4609ad13c92484d
eediallo <elhhabdouldiallo@gmail.com>
2025-03-17 10:48:49 +0000

    feat: add database connection and initialize server startup logic

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,5 +1,9 @@
-import express, { application } from "express";
+import express from "express";
+import { connectDB } from "./db/connection.js";
 import { tasksRouter } from "./routes/tasks.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 
 // parse json
@@ -7,11 +11,18 @@ app.use(express.json())
 
 const port = 3000;
 
-tasksRouter.get('/hello', (req, res)=> {
-res.send('Task Manager App')
-})
-
 // setup tasks router
 app.use('/api/v1/tasks', tasksRouter)
 
-app.listen(port, () => console.log(`Sever is listening on port ${port}...`));
+// ensure that connection to db is successful before running the server
+const start = async ()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => console.log(`Sever is listening on port ${port}...`));
+    }catch(err){
+
+    }
+}
+
+start()
+
