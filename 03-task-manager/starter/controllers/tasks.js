d63163785944b98b172a82c66679810ec45aa015
eediallo <elhhabdouldiallo@gmail.com>
2025-03-17 16:17:46 +0000

    feat: refactor deleteTask to use async/await and improve error handling

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -70,16 +70,19 @@ const updateTask = async (req, res) => {
 };
 
 // delete task
-const deleteTask = (req, res) => {
-  const { id } = req.params;
-  const task = tasks.find((t) => t.id === Number(id));
-  if (!task) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `Task with ${id} not found` });
+const deleteTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  try {
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ success: false, msg: `Task with id ${taskID} not found` });
+    }
+    res.status(200).json({ success: true, task: task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
   }
-  const updatedTasks = tasks.filter((t) => t.id !== Number(id));
-  res.status(201).json({ success: false, tasks: updatedTasks });
 };
 
 export { getAllTasks, createTask, getSingleTask, updateTask, deleteTask };
