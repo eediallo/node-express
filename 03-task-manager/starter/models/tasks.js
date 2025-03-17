a944194df4a6c363ace4687c2d380308558ef1f9
eediallo <elhhabdouldiallo@gmail.com>
2025-03-17 11:17:23 +0000

    feat: enhance Task schema with validation and default values

--- 03-task-manager/starter/models/tasks.js
+++ 03-task-manager/starter/models/tasks.js
@@ -2,8 +2,16 @@ import mongoose from 'mongoose'
 
 // schema
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, 'Name must not be empty'],
+        trim: true,
+        maxLength: [20, 'Name must be no more than 20 characters'],
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
 // model
