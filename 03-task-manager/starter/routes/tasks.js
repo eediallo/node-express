19a45ecbaeff709a992423626c7ad2e637c61d7e
eediallo <elhhabdouldiallo@gmail.com>
2025-03-17 06:05:41 +0000

    feat: enhance tasks router with CRUD operations for task management

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,9 +1,18 @@
-import express from 'express'
-import { getTasks } from '../controllers/tasks.js'
+import express from "express";
+import {
+  getAllTasks,
+  getSingleTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} from "../controllers/tasks.js";
 
-const tasksRouter = express.Router()
+const tasksRouter = express.Router();
 
-tasksRouter.get('/',getTasks)
+tasksRouter.get("/", getAllTasks);
+tasksRouter.post('/', createTask);
+tasksRouter.put('/:id', updateTask)
+tasksRouter.put('/:id', deleteTask)
+tasksRouter.get('/:id', getSingleTask)
 
-
-export{tasksRouter}
+export { tasksRouter };
