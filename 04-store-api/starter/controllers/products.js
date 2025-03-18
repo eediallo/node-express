d6f46d054db7bd5d7597f800ce975365df8d5960
eediallo <elhhabdouldiallo@gmail.com>
2025-03-18 14:03:13 +0000

    feat: enhance products controller to fetch wooden desk products with error handling

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,17 @@
+import { productModel } from "../models/product.js";
 
-const getAllProducts = async (req, res)=>{
-    res.status(200).json({msg: 'products route'})
-}
-const getAllProductsStatic = async (req, res)=>{
-    res.status(200).json({msg: 'products static route'})
-}
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: "products route" });
+};
+const getAllProductsStatic = async (req, res) => {
+  try {
+    const woodenDeskProducts = await productModel.find({ name: "wooden desk" });
+    res
+      .status(200)
+      .json({ woodenDeskProducts, nbHits: woodenDeskProducts.length });
+  } catch (err) {
+    return res.status(404).json({ msg: "Product not found" });
+  }
+};
 
-
-export {getAllProducts, getAllProductsStatic}
+export { getAllProducts, getAllProductsStatic };
