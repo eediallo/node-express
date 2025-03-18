48c4676794d46b7e5118537509ebd20228ebd76a
eediallo <elhhabdouldiallo@gmail.com>
2025-03-18 14:49:20 +0000

    feat: enhance getAllProducts function to support filtering by feature and company

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,21 @@
 import { productModel } from "../models/product.js";
 
 const getAllProducts = async (req, res) => {
-    console.log(req.query)
   try {
-    const products = await productModel.find(req.query);
-    res
-      .status(200)
-      .json({ products, nbHits: products.length });
+    const queryObject = {}; // send all products if search does not match
+    const { feature, company } = req.query;
+
+    if (feature) {
+      queryObject.feature = feature === "true" ? true : false;
+    }
+
+    if (company) {
+      queryObject.company = company;
+    }
+
+    console.log(queryObject);
+    const products = await productModel.find(queryObject);
+    res.status(200).json({ products, nbHits: products.length });
   } catch (err) {
     return res.status(404).json({ msg: "Product not found" });
   }
