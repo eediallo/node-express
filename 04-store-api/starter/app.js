fdc463ea55af4eb6bcfa1c32ef80068910bd96c9
eediallo <elhhabdouldiallo@gmail.com>
2025-03-18 13:01:30 +0000

    feat: set up Express server with dotenv configuration and error handling middleware

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,36 @@
-console.log('04 Store API')
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import {
+  errorHandlerMiddleware,
+} from "./middleware/error-handler.js";
+import { notFound } from "./middleware/not-found.js";
+import { connectDB } from "./db/connect.js";
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1> Store API</h1><a href="/api/v1/products">Products route</a>');
+});
+
+app.use(errorHandlerMiddleware);
+app.use(notFound);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("Successfully connected to database");
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+start();
