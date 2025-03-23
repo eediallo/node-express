df6c7bc12783baf773879f8651173afe4a1cf427
eediallo <elhhabdouldiallo@gmail.com>
2025-03-23 15:55:54 +0000

    feat: enhance user registration with password hashing using bcrypt

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,7 +1,18 @@
 import { User } from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcryptjs";
 const registerUser = async (req, res) => {
-  const user = await User.create({ ...req.body });
+  const { name, email, password } = req.body;
+  //  // Check if the user has provided all the required fields ---> optional
+  //   if (!name || !email || !password) {
+  //     throw new BadRequestError("Please provide name, email and password");
+  //   }
+
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+  const tempUser = { name, email, password: passwordHash };
+  const user = await User.create({ ...tempUser });
+
   res.status(StatusCodes.CREATED).json({ user });
 };
 const loginUser = async (req, res) => {
