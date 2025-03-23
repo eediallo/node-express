99ef16973404ade901ed9da16880df59d715be2e
eediallo <elhhabdouldiallo@gmail.com>
2025-03-23 17:41:02 +0000

    feat: add JWT token generation upon user registration

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,9 +1,17 @@
 import { User } from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import jsonwebtoken from "jsonwebtoken";
 
 const registerUser = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jsonwebtoken.sign(
+    { userId: user._id },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+  res.status(StatusCodes.CREATED).json({ name: user.name, token });
 };
 const loginUser = async (req, res) => {
   res.send("Login user route");
