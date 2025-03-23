5838d90552f7214557cb0c4d1c9fb172664074cb
eediallo <elhhabdouldiallo@gmail.com>
2025-03-23 12:49:15 +0000

    feat: enhance error handler middleware to use http-status-codes for consistency

--- 05-JWT-Basics/starter/middleware/error-handler.js
+++ 05-JWT-Basics/starter/middleware/error-handler.js
@@ -1,9 +1,10 @@
-import {CustomAPIError} from '../errors/custom-error.js'
+import { CustomAPIError } from "../errors/custom-error.js";
+import { StatusCodes } from "http-status-codes";
 export const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+    return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(500).send('Something went wrong try again later')
-}
-
-
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .send("Something went wrong try again later");
+};
