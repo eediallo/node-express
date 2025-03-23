5540066145cee707aafbaaf0d5f9edae30dc9684
eediallo <elhhabdouldiallo@gmail.com>
2025-03-23 13:28:00 +0000

    feat: add job management and user authentication routes

---
+++ 06-jobs-api/starter/models/Job.js
@@ -0,0 +1,15 @@
+const getAllJobs = async (req, res) => {
+  res.send("get all jobs route");
+};
+
+const createJob = async (req, res) => {
+  res.send("create job route");
+};
+const updateJob = async (req, res) => {
+  res.send("create job route");
+};
+const deleteJob = async (req, res) => {
+  res.send("delete job route");
+};
+
+export { getAllJobs, createJob, updateJob, deleteJob };
