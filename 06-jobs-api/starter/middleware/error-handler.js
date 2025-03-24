ef8954e2e48112c48f63b1f2ec6f6d3fe3591637
eediallo <elhhabdouldiallo@gmail.com>
2025-03-24 06:14:27 +0000

    feat: improve error handling middleware by cleaning up code and ensuring consistent response format

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,11 +1,9 @@
-import StatusCodes from 'http-status-codes'
-import {CustomAPIError} from '../errors/custom-api.js'
+import StatusCodes from "http-status-codes";
+import { CustomAPIError } from "../errors/custom-api.js";
 
 export const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err)
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+    return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
-
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+};
