7f6b5b911e2b3764872edc05151dc6f4f82575d7
eediallo <elhhabdouldiallo@gmail.com>
2025-03-24 10:09:35 +0000

    feat: implement getAllJobs function to retrieve jobs created by the authenticated user

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -1,14 +1,15 @@
 import { Job } from "../models/Job.js";
 import { StatusCodes } from "http-status-codes";
-import { UnauthenticatedError } from "../errors/unauthenticated.js"
+import { UnauthenticatedError } from "../errors/unauthenticated.js";
 import { BadRequestError } from "../errors/bad-request.js";
 
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs route");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
-const createJob =  async (req, res) => {
-  req.body.createdBy = req.user.userId // do this to identifier to user who created the job
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId; // do this to identifier to user who created the job
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
